7267410057383a15bf099af1f1ea7b5d83904b91
adoyij <adoyijeremiah50@gmail.com>
2026-09-16 15:16:54 +0100

    BOSS FIGHT — THE TRANSACTION ENGINE

--- submissions/batch4solu.py
+++ submissions/batch4solu.py
@@ -31,18 +31,34 @@
 # print(can_withdraw)
 # print(balance)
 
-product_price = 100000
-quantity = 10
-discount = 20
-total = 999980
-subtotal = 1000000
-valid_quantity = quantity > 0
-valid_total = total > 0
-valid_discount = discount <= subtotal
-subtotal = product_price * quantity
-total = subtotal - discount
-print(valid_quantity)
-print(valid_discount)
-print(valid_total)
-print(subtotal)
-print(total)
+# 
+# product_price = 100000
+# quantity = 10
+# discount = 20
+# total = 999980
+# subtotal = 1000000
+# valid_quantity = quantity > 0
+# valid_total = total > 0
+# valid_discount = discount <= subtotal
+# subtotal = product_price * quantity
+# total = subtotal - discount
+# print(valid_quantity)
+# print(valid_discount)
+# print(valid_total)
+# print(subtotal)
+# print(total)
+
+
+
+balance = 40000
+deposit = 70000
+withdrawal = 30000
+fee = 1000
+can_the_withdrawal_be_made = (withdrawal > 0) and (withdrawal <= balance)
+balance += deposit - withdrawal - fee
+is_the_final_balance_positive = balance >= 0
+is_the_transaction_fee_reasonable = (fee > 0) and (fee <= 1000)
+print(can_the_withdrawal_be_made)
+print(is_the_transaction_fee_reasonable)
+print(f'Final Balance: {balance}')
+print(is_the_final_balance_positive)
