cd655bb614e63a2b3724fee134d358d5118ee314
adoyij <adoyijeremiah50@gail.com>
2026-09-18 15:07:44 +0100

    THE BIG IDEA

--- Part 1 — Programming & Python Foundations/solutions/batch7solu.py
+++ Part 1 — Programming & Python Foundations/solutions/batch7solu.py
@@ -17,17 +17,48 @@
 # balance = 3000
 # if balance < 50000:
 #     print("balance is below #50,000")
-def conditions_that_check_pricess():
-    price = 5000
-    if price > 3000:
-        print(f"price is greater than #3000")
-    if price < 10000:
-        print("price is less than #1000")
-    if price == 5000:
-        print("price is exactly #5000")
-    if price != 2000:
-        print("price is not #2000")
+# def conditions_that_check_pricess():
+#     price = 5000
+#     if price > 3000:
+#         print(f"price is greater than #3000")
+#     if price < 10000:
+#         print("price is less than #1000")
+#     if price == 5000:
+#         print("price is exactly #5000")
+#     if price != 2000:
+#         print("price is not #2000")
         
-conditions_that_check_pricess()
+# conditions_that_check_pricess()
 
  
+
+ #practice 1 .and 2...
+
+# balance = float(input("balance:"))
+# if balance == 1000 and balance > 0:
+#     print("you  have money")
+# else:
+#     print("your walletis empty")
+
+#practice 3............
+# balance = 75000
+# balance = float(input("balance:"))
+# if balance > 100000:
+#     print("High balance")
+# elif balance > 50000:
+#     print("Medium balance")
+# elif balance > 0:
+#     print("Low balance")
+# else:
+#     print("Empty wallet")
+
+score = float(input("score:"))
+
+if score >= 90:
+    print("Excellent")
+elif score >= 70:
+    print("Good")
+elif score >= 50:
+    print("Pass")
+else:
+    print("Fail")
