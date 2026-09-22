eff4d2b4b383d3c09a7178517f7e48a623d9a8fc
adoyij <adoyijeremiah50@gmail.com>
2026-09-22 08:47:30 +0100

    practice 9

--- Part 1 — Programming & Python Foundations/solutions/batch7solu.py
+++ Part 1 — Programming & Python Foundations/solutions/batch7solu.py
@@ -52,13 +52,74 @@
 # else:
 #     print("Empty wallet")
 
-score = float(input("score:"))
+# score = float(input("score:"))
 
-if score >= 90:
-    print("Excellent")
-elif score >= 70:
-    print("Good")
-elif score >= 50:
-    print("Pass")
-else:
-    print("Fail")
+# if score >= 90:
+#     print("Excellent")
+# elif score >= 70:
+#     print("Good")
+# elif score >= 50:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+# balance = 80000
+
+# if balance > 100000:
+#     print("A")
+# elif balance > 50000:
+#     print("B")
+# elif balance > 10000:
+#     print("C")
+# else:
+#     print("D")
+
+#practice 6.....
+# payment_method = input("Payment method ")
+# if payment_method == "cash" or payment_method == "card":
+#    print("Payment method accepted.")
+
+
+#    Let's build a store discount system. practice  7 .......
+
+# ```python
+# total = float(input("Enter total: "))
+
+# if total >= 100000:
+#     print("Large purchase")
+# elif total >= 50000:
+#     print("Medium purchase")
+# elif total >= 10000:
+#     print("Small purchase")
+# else:
+#     print("Very small purchase")
+
+#practice 8........
+# order = float(input("order"))
+# order  == 2000000
+# if order >= 100000:
+#     print("large order")
+# if order >= 50000:
+#     print("medium order")
+# if order >= 10000:
+#     print("small order")
+# else:
+#     print("basic order")
+
+#practice9 ........
+# balance = 50000
+# withdraw = 20000
+# if balance > withdraw:
+#    print("can withdraw")
+# if withdraw <= balance:
+#    print("can withdraw")
+# else:
+#     print("invalid transaction")
+acqual vavuel for balance is 50000
+acqual valve for withdraw is 20000
+balance is equal to 50000
+withdraw is equal to 20000
+the final balance after withdrawal is 30000
+withdraw can only happen when, withdraw is less than the avialable ballance
+if the withdraw is higher than the  avialable balance withdraw will Fail
+the bollean result that the expresion roduce is true because the condision given to the program is true
